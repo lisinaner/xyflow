01852bc045897380c76f5399048276443fe701e1
lsn <1064404481@qq.com>
2024-09-30 09:38:14 +0800

    Update StoreContext.ts

--- packages/react/src/contexts/StoreContext.ts
+++ packages/react/src/contexts/StoreContext.ts
@@ -1,7 +1,7 @@
 import { createContext } from 'react';
 
 import { createStore } from '../store';
-
+//根据react context 提供给其他程序的上下文
 const StoreContext = createContext<ReturnType<typeof createStore> | null>(null);
 
 export const Provider = StoreContext.Provider;
