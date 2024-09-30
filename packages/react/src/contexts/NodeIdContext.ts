555b596f72cf20f38a2425d82c6cb2de86081aca
lsn <1064404481@qq.com>
2024-09-30 10:22:23 +0800

    Update NodeIdContext.ts

--- packages/react/src/contexts/NodeIdContext.ts
+++ packages/react/src/contexts/NodeIdContext.ts
@@ -1,3 +1,4 @@
+//这里从 react 库中导入了 createContext 和 useContext。createContext 用于创建上下文，而 useContext 是一个 Hook，用于在函数组件中访问上下文的值。
 import { createContext, useContext } from 'react';
 
 export const NodeIdContext = createContext<string | null>(null);
