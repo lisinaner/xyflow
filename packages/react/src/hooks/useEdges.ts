ceab516f5d6b01acdccd4ffc3377e59fcb2a14cf
lsn <1064404481@qq.com>
2024-09-30 09:20:32 +0800

    Update useEdges.ts

--- packages/react/src/hooks/useEdges.ts
+++ packages/react/src/hooks/useEdges.ts
@@ -10,6 +10,7 @@ const edgesSelector = (state: ReactFlowState) => state.edges;
  *
  * @public
  * @returns An array of edges
+ * 所有钩子的数据来源都来自zustand的状态管理,里面的返回值
  */
 export function useEdges<EdgeType extends Edge = Edge>(): EdgeType[] {
   const edges = useStore(edgesSelector, shallow) as EdgeType[];
